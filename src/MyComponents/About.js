84eafc10eedf5734fb986f7346f96921dfa44028
Mojaffar Ansari <mojaffaransari2016@gmail.com>
2021-06-11 15:54:28 +0530

    deploy my react app to github

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className="container my-3 active">
+            <h3>My First Project On React</h3>
+            <p>Hello, This is my first React App project. I've built this from scratch. We can use it to add Todos list and can delete todos from the list. Only basic operations, did not use any database.Hope you like it! Thanks!</p>
+        </div>
+    )
+}
